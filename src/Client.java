326cf0f8e24aad52e7556ec03e2538a7fd53d12a
PLatchenkov <baldr1992@gmail.com>
2018-03-16 11:09:10 +0300

    old files

--- src/Client.java
+++ src/Client.java
@@ -7,7 +7,7 @@ public class Client {
     public void go() {
         try {
             Socket socket = new Socket("127.0.0.1", 4242);
-            BufferedReader 
+            BufferedReader
         } catch (IOException e) {
             e.printStackTrace();
         }
